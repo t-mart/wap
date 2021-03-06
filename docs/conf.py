1dba67e2bf3aaa86c2cef8e535e5984fb4da9229
Tim Martin <tim@timmart.in>
2021-03-06 09:17:46 -0600

    Bump version: 0.8.0 → 0.8.1

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = "2021, Tim Martin"
 author = "Tim Martin"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.8.1"
 
 
 # -- General configuration ---------------------------------------------------
