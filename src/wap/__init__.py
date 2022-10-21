0f6f32928874cd5bc17aa98cc4948eec448eb9da
Tim Martin <tim@timmart.in>
2022-10-21 17:28:42 -0500

    Add __name__ note

--- src/wap/__init__.py
+++ src/wap/__init__.py
@@ -1,4 +1,5 @@
 from importlib.metadata import version
 
+# this needs to match the package name that's installed, even if its binary is different
 __name__ = "wow-addon-packager"
 __version__ = version(__name__)
